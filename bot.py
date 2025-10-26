c434e71a1ccecaf1b3ee6ca44aeb1f2407006596
Unai Tolosa Pontesta <tolosa@ni.eus>
2025-10-26 00:39:42 +0000

    update: xiberokobotza.eus

--- bot.py
+++ bot.py
@@ -14,66 +14,45 @@ MASTODON_ACCESS_TOKEN = os.getenv('MASTODON_ACCESS_TOKEN')
 MASTODON_URL = 'https://mastodon.eus'
 tmp_img = '/tmp/image.png'
 def get_news():
-    url = f'https://www.xiberokobotza.org/berriak'
+    albisteak=[]
+    url = f'https://xiberokobotza.eus/aktualitatea'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    results = soup.find(id="sppb-addon-wrapper-1573106262965")
-    job_elements = results.find_all("div", class_="sppb-addon-article")
-    albisteak=[]
+    results = soup.find(id="highlights-content")
+    
+    left_element = results.find("div", id="left-column")
+    albiste1=get_albistea(left_element)
+    albisteak.append(albiste1)
 
-  #  print(job_elements)
-    for job_element in job_elements:
-        albistea=[]
-        print(job_element)
-        title_element = job_element.find("h3")
-        print(title_element)
-        desk_element  = job_element.find("div", class_="sppb-article-introtext").text
-        print(desk_element)
-        data_element  = job_element.find("span", class_="sppb-meta-date")
-        print(data_element)
-        img_element   = job_element.find_all("img")
-        print(img_element[0])
-        if len(img_element) == 0:
-            img_element   = job_element.find_all("img")
-            albistea.append("https://www.xiberokobotza.org"+img_element[0]['src'])
-        else:
-            albistea.append("https://www.xiberokobotza.org"+img_element[0]['src'])
-        url_element   = title_element.find(href=True)
-        data_fomateatuta= date_eu_to_en(data_element.text)
-        new_datatime = datetime.strptime(data_fomateatuta, '%Y %B %d')
-        albistea.append(title_element.text.strip()+ "\n" +"https://www.xiberokobotza.org"+ url_element['href'])
-        albistea.append(new_datatime)
-        albisteak.append(albistea)
+    right_element = results.find("div", id="right-column")
+    albiste2=get_albistea(right_element)
+    albisteak.append(albiste2)
         
     return albisteak
-    
-def date_eu_to_en(date):
-    data_list=date.split(" ")
-    if data_list[1] == "Urtarrila":
-        return data_list[0]+" January "+data_list[2]
-    elif data_list[1] == "Otsaila":
-        return data_list[0]+" February "+data_list[2]
-    elif data_list[1] == "Martxoa":
-        return data_list[0]+" March "+data_list[2]
-    elif data_list[1] == "Apirila":
-        return data_list[0]+" April "+data_list[2]
-    elif data_list[1] == "Maiatza":
-        return data_list[0]+" May "+data_list[2]
-    elif data_list[1] == "Ekaina":
-        return data_list[0]+" June "+data_list[2]
-    elif data_list[1] == "Uztaila":
-        return data_list[0]+" July "+data_list[2]
-    elif data_list[1] == "Abuztua":
-        return data_list[0]+" August "+data_list[2]
-    elif data_list[1] == "Iraila":
-        return data_list[0]+" September "+data_list[2]
-    elif data_list[1] == "Urria":
-        return data_list[0]+" October "+data_list[2]
-    elif data_list[1] == "Azaroa":
-        return data_list[0]+" November "+data_list[2]
+
+def get_albistea(element):
+
+    albistea=[]
+    #print(element)
+    albiste1_url=element.find_all("a")[0]['href']
+    response1 = requests.get(albiste1_url)
+    soup1 = BeautifulSoup(response1.content, "html.parser")
+    data = soup1.find("h5",id="egilea-data").text.strip()
+
+    new_datatime = datetime.strptime(data, '%Y/%m/%d %H:%M')
+
+    title_element = soup1.find("h1",id="title")
+    img_element   = soup1.find_all("img")
+    #print(img_element[0])
+    if len(img_element) == 0:
+        img_element   = soup1.find_all("img")
+        albistea.append("https://www.xiberokobotza.eus"+img_element[0]['src'])
     else:
-        return data_list[0]+" December "+data_list[2]
-    
+        albistea.append("https://www.xiberokobotza.eus"+img_element[0]['src'])
+
+    albistea.append(title_element.text.strip()+ "\n" +albiste1_url)
+    albistea.append(new_datatime)
+    return albistea    
 
 async def send_message(message):
     bot = Bot(token=TELEGRAM_BOT_TOKEN)
